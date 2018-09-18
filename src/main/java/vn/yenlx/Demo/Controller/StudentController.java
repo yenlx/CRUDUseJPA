522d20c65846dcb33f6f69444d3dff8e34bd43b4
yenlx <yenlx@vnext.com.vn>
2018-09-18 17:34:58 +0700

    update method

--- src/main/java/vn/yenlx/Demo/Controller/StudentController.java
+++ src/main/java/vn/yenlx/Demo/Controller/StudentController.java
@@ -3,8 +3,9 @@ package vn.yenlx.Demo.Controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,5 +22,12 @@ public class StudentController {
 	@GetMapping(value = "/getStudent")
 	public List<Student> getStudent(){
 		return services.getLstStudent();
+	}
+	
+	@PostMapping(value = "/addStudent")
+	public void addStudent(@ModelAttribute Student student) {
+		String a ="123";
+		return;
+				
 	}
 }
